3bbadcf28e5ebd6295bfce28ec841ae48e8d01fd
Egor <gurskijegor88@gmail.com>
2025-10-30 21:21:48 +0300

    first commit

--- task-3/main.py
+++ task-3/main.py
@@ -1,4 +1,4 @@
 #Egor
-with open("task-3\\text.txt","r") as file:#открытие файла
+with open("text.txt","r") as file:#открытие файла
     lines=file.readlines()#нахождение количества строк в файле
 print(f"В файле {len(lines)} строки")
